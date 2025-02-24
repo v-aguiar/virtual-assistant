32b09222ac1c70d74302e56dc662002ca9bd4738
Victor Aguiar Ribeiro <v.aguiar.dev@gmail.com>
2025-02-23 22:39:32 -0300

    fix: remove condition

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ from open_youtube import open_yt
 def main():
     while True:
         command = listen()
-        print(f"Você disse agora: {command}")  # Adicionando o print para debug
 
         if command:
             command = command.strip().lower()
-
-            print(f"Comando processado: '{command}'")
 
             if 'oi' in command:
                 speak("Olá! Como posso ajudar?")
@@ -24,9 +21,6 @@ def main():
                 speak("Abrindo o YouTube.")
                 result = open_yt()
                 speak(result)
-            elif 'localização da farmácia mais próxima' in command:
-                speak("Mostrando a localização da farmácia mais próxima.")
-                # Adicione o código para mostrar a localização
             elif 'sair' in command:
                 speak("Saindo.")
                 break
